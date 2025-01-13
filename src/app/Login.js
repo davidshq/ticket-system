b9076870eb0c064da692bb6f09c221a2d783a83f
Dave Mackey <dave@davemackey.net>
2025-01-13 17:52:03 -0500

    Wire up login form for password and magic link

--- src/app/Login.js
+++ src/app/Login.js
@@ -1,9 +1,72 @@
-export const Login = () => {
-  return(
-    <form>
-      <article style={{ maxWidth: '420px', margin: 'auto' }}>
+"use client";
+import { useRef } from "react";
+import Link from "next/link";
+
+export const Login = ({ isPasswordLogin }) => {
+  const emailInputRef = useRef(null);
+  const passwordInputRef = useRef(null);
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (isPasswordLogin) {
+          alert("User wants to login with password");
+        } else {
+          alert("User wants to login with magic link");
+        }
+      }}
+    >
+      <article style={{ maxWidth: "420px", margin: "auto" }}>
         <header>Login</header>
-        <strong>Hello Login!</strong>
+        <fieldset>
+          <label htmlFor="email">
+            Email
+            <input
+              ref={emailInputRef}
+              type="email"
+              id="email"
+              name="email"
+              required
+            />
+          </label>
+          {isPasswordLogin && (
+            <label htmlFor="password">
+              Password
+              <input
+                ref={passwordInputRef}
+                type="password"
+                id="password"
+                name="password"
+              />
+            </label>
+          )}
+        </fieldset>
+        <p>
+          {isPasswordLogin ? (
+            <Link
+              href={{
+                pathname: "/",
+                query: { magicLink: "yes" },
+              }}
+            >
+              Go to Magic Link Login
+            </Link>
+          ) : (
+            <Link
+              href={{
+                pathname: "/",
+                query: { magicLink: "no" },
+              }}
+            >
+              Go to Password Login
+            </Link>
+          )}
+        </p>
+        <button type="submit">
+          Sign in with
+          {isPasswordLogin ? " Password" : "Send Magic Link"}
+        </button>
       </article>
     </form>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 import { Login } from "./Login";
-export default function LoginPage() {
-  return <Login />;
+export default async function LoginPage({ searchParams }) {
+  const params = await searchParams;
+  const wantsMagicLink = params.magicLink === "yes";
+  return <Login isPasswordLogin={!wantsMagicLink} />;
 }
