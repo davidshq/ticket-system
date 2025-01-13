1dd62f5c905b1ea9586f842266c63ab516ce0ab9
Dave Mackey <dave@davemackey.net>
2025-01-13 16:55:55 -0500

    Add pico.css, login component

---
+++ src/app/Login.js
@@ -0,0 +1,10 @@
+export const Login = () => {
+  return(
+    <form>
+      <article style={{ maxWidth: '420px', margin: 'auto' }}>
+        <header>Login</header>
+        <strong>Hello Login!</strong>
+      </article>
+    </form>
+  );
+};
--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,4 @@
-"use client";
-
-import Image from "next/image";
-import { useEffect } from "react";
-import { getSupabaseBrowserClient } from '@/supabase-utils/browserClient';
-import styles from "./page.module.css";
-
-export default function Home() {
-  useEffect(() => {
-    const supabase = getSupabaseBrowserClient();
-    supabase.storage.listBuckets().then((result) => {
-      console.log("Bucket List", result)
-    });
-  }, []);
-
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-      </main>
-    </div>
-  );
+import { Login } from "./Login";
+export default function LoginPage() {
+  return <Login />;
 }
