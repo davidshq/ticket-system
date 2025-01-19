0ce69ed8984b87b6b5f19c80741cf5ea03d7f622
Dave Mackey <dave@davemackey.net>
2025-01-19 09:32:31 -0500

    Update nav, add user list page

--- src/app/tickets/Nav.js
+++ src/app/tickets/Nav.js
@@ -1,16 +1,22 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function Nav() {
+  const pathname = usePathname();
+  const activeProps = { className: "contrast" };
+  const inactiveProps = { className: "secondary outline" };
   return(
     <nav>
       <ul>
         <li>
-          <Link role="button" href="/tickets">Ticket List</Link>
+          <Link role="button" href="/tickets" {...(pathname === "/tickets" ? activeProps : inactiveProps)}>Ticket List</Link>
         </li>
         <li>
-          <Link role="button" href="/tickets/new">New Ticket</Link>
+          <Link role="button" href="/tickets/new" {...(pathname === "/tickets/new" ? activeProps : inactiveProps)}>New Ticket</Link>
         </li>
         <li>
-          <Link role="button" href="/tickets/users">User List</Link>
+          <Link role="button" href="/tickets/users" {...(pathname === "/tickets/users" ? activeProps : inactiveProps)}>User List</Link>
         </li>
       </ul>
       <ul>
